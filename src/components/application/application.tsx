4d5d4e0928151bcef86a48e02922462b07461d0f
Alex Bezborodov <alexbezborodov23@gmail.com>
2022-10-09 23:11:05 +0300

    Feature: create application component

---
+++ src/components/application/application.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../../App.css";
+import { Header } from "../header/header";
+import { Layout } from "../layout";
+
+export function Application() {
+  return (
+    <>
+      <Header />
+      <Layout />
+    </>
+  );
+}
