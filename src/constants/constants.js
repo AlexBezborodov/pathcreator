0798bef1205e38e92615d47057f93c7bfbb943ca
Alex Bezborodov <alexbezborodov23@gmail.com>
2022-10-11 22:49:59 +0300

    Feature: update mockData

---
+++ src/constants/constants.js
@@ -0,0 +1,57 @@
+export const LOGO =
+  "https://1000logos.net/wp-content/uploads/2022/01/Polestar-logo.png";
+
+export const mockData = [
+  {
+    id: 1,
+    title: "test11",
+    shortDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    fullDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    result: 1.2,
+    isFavourite: false,
+    coords: [
+      { lat: 49.44539, lng: 32.061158 },
+      { lat: 49.44539, lng: 32.061158 },
+    ],
+  },
+  {
+    id: 2,
+    title: "test2",
+    shortDescription: "test222",
+    fullDescription: "test222 full",
+    result: 3.2,
+    isFavourite: true,
+    coords: [
+      { lat: 49.44539, lng: 32.061158 },
+      { lat: 49.44539, lng: 32.061158 },
+    ],
+  },
+  {
+    id: 3,
+    title: "test3",
+    shortDescription: "test333",
+    fullDescription: "test333 full",
+    result: 4.7,
+    isFavourite: false,
+    coords: [
+      { lat: 49.44539, lng: 32.061158 },
+      { lat: 49.44539, lng: 32.061158 },
+    ],
+  },
+  {
+    id: 4,
+    title: "My favourite path",
+    shortDescription:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+    fullDescription:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. FULL",
+    result: 4.7,
+    isFavourite: true,
+    coords: [
+      { lat: 49.44539, lng: 32.061158 },
+      { lat: 49.44539, lng: 32.061158 },
+    ],
+  },
+];
