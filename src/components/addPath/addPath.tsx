9a04f787f7a951976c7871fe7f9c0f409f4643c4
Alex Bezborodov <alexbezborodov23@gmail.com>
2022-10-12 20:21:26 +0300

    Feature: add logic for updating markers

--- src/components/addPath/addPath.tsx
+++ src/components/addPath/addPath.tsx
@@ -2,94 +2,141 @@ import React, { FC, useState } from "react";
 
 import { FundOutlined } from "@ant-design/icons";
 import { Modal, Input, Row, Col, Button } from "antd";
-
-import { AddPathProps, DataPath } from "../../interfaces";
-import { Map } from "../map";
 import { observer } from "mobx-react-lite";
+
+import { AddPathProps, DataPath, Marker } from "../../interfaces";
 import pathsStore from "../../store/paths";
+import { Map } from "../map";
 
 const { TextArea } = Input;
 
-export const AddPath: FC<any> = observer(({
-  isModalOpen,
-  setIsModalOpen,
-}: AddPathProps) => {
-  const [data, setData] = useState<DataPath | any>({});
-  const onHandleChange = (event: any) => {
-    setData((prev: Object) => ({ ...prev, [event.target.name]: event.target.value }));
-  };
+export const AddPath: FC<any> = observer(
+  ({ isModalOpen, setIsModalOpen }: AddPathProps) => {
+    const [data, setData] = useState<DataPath | any>({});
+    const [markers, setMarkers] = useState<any>([]);
+    const onHandleChange = (event: any) => {
+      setData((prev: Object) => ({
+        ...prev,
+        [event.target.name]: event.target.value,
+      }));
+    };
 
-  const handleCancel = () => {
-    setIsModalOpen(false);
-    setData({});
-  };
+    const addMarker = () => {
+      const defaultMarker = {
+        id: Math.floor(Math.random() * 10000) + 1,
+        position: { lat: 49.44539, lng: 32.061158 },
+      };
+      setMarkers([...markers, defaultMarker]);
+    };
 
-  const onAddPath = () => {
-    const newPath = {
-      id: Math.floor(Math.random() * 10000) + 1,
-      ...data,
-      isFavourite: false,
-      result: 4.4,
-      coords: [
-        { lat: 49.44539, lng: 32.061158 },
-        { lat: 49.44539, lng: 32.061158 },
-      ],
-    }
-    pathsStore.addPath(newPath);
-    handleCancel();
-  }
+    const handleCancel = () => {
+      setIsModalOpen(false);
+      setData({});
+      setMarkers([]);
+    };
 
-  return (
-    <>
-      <Modal
-        title="Add new path"
-        open={isModalOpen}
-        onCancel={handleCancel}
-        width="90vw"
-        style={{ maxWidth: "1200px" }}
-        footer={null}
-      >
-        <Row justify="center">
-          <Col className="col revert-col" xs={24} md={24} lg={12}>
-            <Input
-              placeholder="Title"
-              name="title"
-              value={data?.title || ""}
-              onChange={onHandleChange}
-            />
-            <div style={{ margin: "24px 0" }} />
-            <TextArea
-              showCount
-              maxLength={100}
-              placeholder="Short description"
-              autoSize={{ minRows: 5, maxRows: 5 }}
-              name="shortDescription"
-              onChange={onHandleChange}
-              value={data?.shortDescription || ""}
-            />
-            <div style={{ margin: "24px 0" }} />
-            <TextArea
-              placeholder="Controlled autosize"
-              autoSize={{ minRows: 7, maxRows: 7 }}
-              name="fullDescription"
-              value={data?.fullDescription || ""}
-              onChange={onHandleChange}
-            />
-            <p style={{ textAlign: "center", margin: "1.5rem 0" }}>
-              <FundOutlined style={{ fontSize: "20px", marginRight: "10px" }} />{" "}
-              Total length 4.4km
-            </p>
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <Button type="primary" size="large" onClick={onAddPath} disabled={!data.title || !data.shortDescription || !data.fullDescription}>
-                Add path
-              </Button>
-            </div>
-          </Col>
-          <Col className="revert-col" xs={24} md={24} lg={12}>
-            <Map maxHeight="520px" />
-          </Col>
-        </Row>
-      </Modal>
-    </>
-  );
-});
+    const onAddPath = () => {
+      const newPath = {
+        id: Math.floor(Math.random() * 10000) + 1,
+        ...data,
+        isFavourite: false,
+        result: 4.4,
+        defaultCoords: { lat: 49.44539, lng: 32.061158 },
+        markers,
+      };
+      pathsStore.addPath(newPath);
+      handleCancel();
+    };
+
+    const updateMarker = (updatedMarker: Marker) => {
+      const updMarkers = markers.map((item: Marker) =>
+        item.id === updatedMarker.id ? { ...item, ...updatedMarker } : item,
+      );
+      setMarkers(updMarkers);
+    };
+
+    return (
+      <>
+        <Modal
+          title="Add new path"
+          open={isModalOpen}
+          onCancel={handleCancel}
+          width="90vw"
+          style={{ maxWidth: "1200px" }}
+          footer={null}
+        >
+          <Row justify="center">
+            <Col className="col revert-col" xs={24} md={24} lg={12}>
+              <Input
+                placeholder="Title"
+                name="title"
+                value={data?.title || ""}
+                onChange={onHandleChange}
+              />
+              <div style={{ margin: "24px 0" }} />
+              <TextArea
+                showCount
+                maxLength={100}
+                placeholder="Short description"
+                autoSize={{ minRows: 5, maxRows: 5 }}
+                name="shortDescription"
+                onChange={onHandleChange}
+                value={data?.shortDescription || ""}
+              />
+              <div style={{ margin: "24px 0" }} />
+              <TextArea
+                placeholder="Controlled autosize"
+                autoSize={{ minRows: 7, maxRows: 7 }}
+                name="fullDescription"
+                value={data?.fullDescription || ""}
+                onChange={onHandleChange}
+              />
+              <p style={{ textAlign: "center", margin: "1.5rem 0" }}>
+                <FundOutlined
+                  style={{ fontSize: "20px", marginRight: "10px" }}
+                />{" "}
+                Total length 4.4km
+              </p>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <Button
+                  type="primary"
+                  size="large"
+                  onClick={onAddPath}
+                  disabled={
+                    !data.title ||
+                    !data.shortDescription ||
+                    !data.fullDescription ||
+                    markers.length < 2
+                  }
+                >
+                  Add path
+                </Button>
+              </div>
+            </Col>
+            <Col className="revert-col" xs={24} md={24} lg={12}>
+              <div style={{ position: "relative" }}>
+                <Button
+                  type="primary"
+                  onClick={addMarker}
+                  style={{
+                    position: "absolute",
+                    top: "10px",
+                    left: "45%",
+                    zIndex: 2,
+                  }}
+                >
+                  Add marker
+                </Button>
+                <Map
+                  maxHeight="520px"
+                  markers={markers}
+                  updateMarker={updateMarker}
+                />
+              </div>
+            </Col>
+          </Row>
+        </Modal>
+      </>
+    );
+  },
+);
--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
 import { MapProps } from "../../interfaces";
 
@@ -8,25 +8,52 @@ export const Map: FC<any> = ({
   width = "100%",
   height = "80vh",
   maxHeight = "420px",
-  coords = { lat: 49.44539, lng: 32.061158 },
+  defaultCoords = { lat: 49.44539, lng: 32.061158 },
+  markers = [],
+  updateMarker,
 }: MapProps) => {
   const containerStyle = {
     width,
     height,
     maxHeight,
   };
-
-  const center = { lat: coords.lat, lng: coords.lng };
+  const center = { lat: defaultCoords.lat, lng: defaultCoords.lng };
 
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
-    googleMapsApiKey: "AIzaSyDKYMmcGqXyl3quJKk-VefGsQbDxxNJSk0",
+    googleMapsApiKey: "AIzaSyBOekKhmcij4YvQjsagQcM7-1tt4VzJw5o",
   });
+
+  const getPosition = (e: any, id: number) => {
+    const updatedMarker = {
+      id,
+      position: { lat: e.latLng.lat(), lng: e.latLng.lng() },
+    };
+    updateMarker(updatedMarker);
+  };
   return (
     <div style={{ height: "100%", width: "100%" }}>
       {isLoaded ? (
-        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={17}>
-          {/* Child components, such as markers, info windows, etc. */}
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={17}
+          options={{
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: false,
+          }}
+        >
+          {markers.length &&
+            markers.map((marker) => (
+              <Marker
+                key={marker.id}
+                position={marker.position}
+                draggable={true}
+                onDragEnd={(e) => getPosition(e, marker.id)}
+              />
+            ))}
+
           <></>
         </GoogleMap>
       ) : null}
