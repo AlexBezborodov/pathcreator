4afc77cf11cdafbca865f8bfad9c0613141f370f
Alex Bezborodov <alexbezborodov23@gmail.com>
2022-10-12 22:48:51 +0300

    Feature: add calculation distance func

--- src/components/addPath/addPath.tsx
+++ src/components/addPath/addPath.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 
 import { FundOutlined } from "@ant-design/icons";
 import { Modal, Input, Row, Col, Button } from "antd";
@@ -20,7 +20,6 @@ export const AddPath: FC<any> = observer(
         [event.target.name]: event.target.value,
       }));
     };
-
     const addMarker = () => {
       const defaultMarker = {
         id: Math.floor(Math.random() * 10000) + 1,
@@ -40,12 +39,30 @@ export const AddPath: FC<any> = observer(
         id: Math.floor(Math.random() * 10000) + 1,
         ...data,
         isFavourite: false,
-        result: 4.4,
         defaultCoords: { lat: 49.44539, lng: 32.061158 },
         markers,
       };
       pathsStore.addPath(newPath);
       handleCancel();
+    };
+    const calculateDistance = () => {
+      if (markers.length === 2) {
+        const start = new google.maps.LatLng(
+          markers[0].position.lat,
+          markers[0].position.lng,
+        );
+
+        const end = new google.maps.LatLng(
+          markers[1].position.lat,
+          markers[1].position.lng,
+        );
+
+        const distance = (
+          google.maps.geometry.spherical.computeDistanceBetween(start, end) /
+          1000
+        ).toFixed(2);
+        setData((prev: Marker) => ({ ...prev, result: distance }));
+      }
     };
 
     const updateMarker = (updatedMarker: Marker) => {
@@ -54,6 +71,10 @@ export const AddPath: FC<any> = observer(
       );
       setMarkers(updMarkers);
     };
+
+    useEffect(() => {
+      calculateDistance();
+    }, [markers]);
 
     return (
       <>
@@ -95,7 +116,7 @@ export const AddPath: FC<any> = observer(
                 <FundOutlined
                   style={{ fontSize: "20px", marginRight: "10px" }}
                 />{" "}
-                Total length 4.4km
+                Total length {data?.result || "- "}km
               </p>
               <div style={{ display: "flex", justifyContent: "center" }}>
                 <Button
@@ -118,6 +139,7 @@ export const AddPath: FC<any> = observer(
                 <Button
                   type="primary"
                   onClick={addMarker}
+                  disabled={markers.length >= 2}
                   style={{
                     position: "absolute",
                     top: "10px",
